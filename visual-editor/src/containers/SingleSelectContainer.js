49cb05b7af1d68b94db55539032485eaa934854c
ihor-romaniuk <Igorek25>
2019-07-17 13:46:52 +0300

    style tinymce editor

--- visual-editor/src/containers/SingleSelectContainer.js
+++ visual-editor/src/containers/SingleSelectContainer.js
@@ -17,13 +17,34 @@ export class SingleSelectContainer extends React.Component {
     render() {
         return (
             <div className="lxc-unit-editor-wrapper">
-                <Editor
-                    className="lxc-advanced-settings-block"
-                    apiKey='283hsctoygj8rdat1mccsgurzgph73mg3pdgu0lc7j9wq6vr'
-                    onChange={this.handleEditorChange.bind(this)}
-                    initialValue={this.props.editorContent}
-                    value={this.props.editorContent}
-                />
+                <fieldset className="lxc-answers-wrapper">
+                    <legend className="lxc-answers-title">Question*</legend>
+                    <div className="lxc-answers-description">
+                        Create a question that only has one answer.
+                    </div>
+                    <Editor
+                        init={{
+                            menubar:false,
+                            statusbar: false,
+                            plugins:"link code image",
+                            toolbar: "formatselect bold italic | code blockquote link image | undo redo",
+                            height: 392,
+                            // icons_url: "../assets/icons/lxcicons/icons.js",
+                            // icons: "lxcicons",
+                            mobile: {
+                                theme: "silver",
+                                height: 490,
+                                plugins:"link code image",
+                                toolbar: "formatselect | bold italic | code blockquote link image | undo redo",
+                            }
+                        }}
+                        className="lxc-advanced-settings-block"
+                        apiKey='283hsctoygj8rdat1mccsgurzgph73mg3pdgu0lc7j9wq6vr'
+                        onChange={this.handleEditorChange.bind(this)}
+                        initialValue={this.props.editorContent}
+                        value={this.props.editorContent}
+                    />
+                </fieldset>
                 <SingleSelectAnswers 
                     answersList={this.props.answersList}
                     singleSelectChangeAnswer={this.props.singleSelectChangeAnswer} 
