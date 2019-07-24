c66096d4b0ce99f99b50d7bdcf3d94110842d11b
Alexander Marenich <sendr84@gmail.com>
2019-07-24 18:17:15 +0300

    copy images to studio if CONFIG_ROOT is set

--- visual-editor/gulpfile.js
+++ visual-editor/gulpfile.js
@@ -3,13 +3,15 @@ const sass = require('gulp-sass');
 const concat       = require('gulp-concat');
 const cleanCss     = require('gulp-clean-css');
 const autoprefixer    = require('gulp-autoprefixer');
-const copy         = require('gulp-copy');
+const exec = require('child_process').exec;
 const replace = require('gulp-replace');
 const clean        = require('gulp-clean');
+const nop = require('gulp-nop');
 
 const path = require('path');
 
 const static = path.resolve(__dirname);
+const studioStatic = path.resolve(process.env.CONFIG_ROOT || '', 'edx-platform/cms/static/images');
 
 gulp.task('pack-css', function () {
     return gulp.src(['build/static/css/*'])
@@ -35,8 +37,11 @@ gulp.task('pack-scss', function () {
 });
 
 gulp.task('copy-images', function () {
-    return gulp.src('build/static/media/*')
-           .pipe(copy('assets/images', {prefix:3}));
+    if (process.env.CONFIG_ROOT !== undefined) {
+        return gulp.src('build/static/media/*')
+               .pipe(gulp.dest(studioStatic));
+    }
+    return gulp.src('.').pipe(nop());
 });
 
 gulp.task('sass:watch', function () {
@@ -52,4 +57,12 @@ gulp.task('clean-bundle', function () {
         }));
 });
 
-gulp.task('default', gulp.series('pack-css', 'pack-js', 'copy-images', 'clean-bundle'));
+gulp.task('build', function (cb) {
+    return exec('npm run build', (err, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+gulp.task('default', gulp.series('build', 'pack-css', 'pack-js', 'copy-images', 'clean-bundle'));
